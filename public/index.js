96d9c193f66d4082dcbbb5b245e2b54cc1962526
João Vitor Meireles <joaovmm12072005@gmail.com>
2025-05-14 11:06:28 -0300

    curso 2 de websockets

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-import { emitirAdcionarDocumento } from "./socket-front-index.js";
+import { emitirAdicionarDocumento } from "./socket-front-index.js";
 
 const listaDocumentos = document.getElementById("lista-documentos");
 const form = document.getElementById("form-adiciona-documento");
@@ -6,27 +6,26 @@ const inputDocumento = document.getElementById("input-documento");
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
-  emitirAdcionarDocumento(inputDocumento.value);
+  emitirAdicionarDocumento(inputDocumento.value);
   inputDocumento.value = "";
 });
 
 function inserirLinkDocumento(nomeDocumento) {
   listaDocumentos.innerHTML += `
-    <a 
-      href="documento.html?nome=${nomeDocumento}" 
+    <a
+      href="/documento/index.html?nome=${nomeDocumento}"
       class="list-group-item list-group-item-action"
       id="documento-${nomeDocumento}"
     >
       ${nomeDocumento}
     </a>
-    `;
+  `;
 }
 
 function removerLinkDocumento(nomeDocumento) {
   const documento = document.getElementById(`documento-${nomeDocumento}`);
 
   listaDocumentos.removeChild(documento);
-  
 }
 
 export { inserirLinkDocumento, removerLinkDocumento };
--- servidor/db/dbConnect.js
+++ servidor/db/dbConnect.js
@@ -1,18 +1,19 @@
 import { MongoClient } from "mongodb";
 
-const cliente = new MongoClient("mongodb+srv://joaovm:joaovm@websocket.n0d7qyj.mongodb.net/?retryWrites=true&w=majority&appName=Websocket");
+const cliente = new MongoClient(
+  "mongodb+srv://alura:123@aluracluster.lp6gdyc.mongodb.net/?retryWrites=true&w=majority"
+);
 
 let documentosColecao;
 
 try {
   await cliente.connect();
 
-  const db = cliente.db("Alurasocket");
+  const db = cliente.db("alura-websockets");
   documentosColecao = db.collection("documentos");
 
   console.log("Conectado ao banco de dados com sucesso!");
-}
-catch (erro) {
+} catch (erro) {
   console.log(erro);
 }
 
