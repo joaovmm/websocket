07db038d6ec7da9ef4483632c4e3565ff4736b79
João Vitor Meireles <joaovmm12072005@gmail.com>
2025-05-19 19:52:14 -0300

    projeto final

--- servidor/db/dbConnect.js
+++ servidor/db/dbConnect.js
@@ -1,20 +1,21 @@
 import { MongoClient } from "mongodb";
 
 const cliente = new MongoClient(
-  "mongodb+srv://alura:123@aluracluster.lp6gdyc.mongodb.net/?retryWrites=true&w=majority"
+  "mongodb+srv://joaovm:joaovm@websocket.n0d7qyj.mongodb.net/?retryWrites=true&w=majority&appName=Websocket"
 );
 
-let documentosColecao;
+let documentosColecao, usuariosColecao;
 
 try {
   await cliente.connect();
 
   const db = cliente.db("alura-websockets");
   documentosColecao = db.collection("documentos");
+  usuariosColecao = db.collection("usuarios");
 
   console.log("Conectado ao banco de dados com sucesso!");
 } catch (erro) {
   console.log(erro);
 }
 
-export { documentosColecao };
+export { documentosColecao, usuariosColecao };
